96159899059cc41a1c0c0cca13ecfce72321be4d
Gastd <gabriel.araujo.5000@gmail.com>
2019-09-18 14:41:13 -0300

    Remove some white spaces

--- iot_gps.c
+++ iot_gps.c
@@ -1,3 +1,4 @@
+#include <czmq.h>
 #include <termios.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -9,7 +10,7 @@
 #define MAX_BYTES  100
 
 uint8_t gps_data_[MAX_BYTES];
-uint8_t time[10];
+uint8_t gps_time[10];
 uint8_t llh[5][10];
 int fdd = 0;
 
@@ -56,8 +57,8 @@ nmea_parser(/* const char * message */)
                 case 1: // utc gps time
                 {
                     end = i;
-                    memcpy(time, &gps_data_[begin], end - begin);
-                    time[end+1] = '\0';
+                    memcpy(gps_time, &gps_data_[begin], end - begin);
+                    gps_time[end+1] = '\0';
                     state++;
                     begin = i+1;
                     break;
@@ -108,7 +109,7 @@ nmea_parser(/* const char * message */)
         }
     }
 
-    printf ("time: %s\n", time);
+    printf ("time: %s\n", gps_time);
     printf ("latitude: %s %s\n", llh[0], llh[1]);
     printf ("logitude: %s %s\n", llh[2], llh[3]);
 
@@ -119,78 +120,89 @@ nmea_parser(/* const char * message */)
 int
 set_interface_attribs (int fd, int speed, int parity)
 {
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
-        if (tcgetattr (fd, &tty) != 0)
-        {
-                printf ("%d error from tcgetattr\n", errno);
-                return -1;
-        }
+    struct termios tty;
+    memset (&tty, 0, sizeof tty);
+    if (tcgetattr (fd, &tty) != 0)
+    {
+            printf ("%d error from tcgetattr\n", errno);
+            return -1;
+    }
 
-        cfsetospeed (&tty, speed);
-        cfsetispeed (&tty, speed);
+    cfsetospeed (&tty, speed);
+    cfsetispeed (&tty, speed);
 
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        tty.c_iflag &= ~IGNBRK;         // ignore break signal
-        tty.c_lflag = 0;                // no signaling chars, no echo,
-                                        // no canonical processing
-        tty.c_oflag = 0;                // no remapping, no delays
-        tty.c_cc[VMIN]  = 0;            // read doesn't block
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+    // disable IGNBRK for mismatched speed tests; otherwise receive break
+    // as \000 chars
+    tty.c_iflag &= ~IGNBRK;         // ignore break signal
+    tty.c_lflag = 0;                // no signaling chars, no echo,
+                                    // no canonical processing
+    tty.c_oflag = 0;                // no remapping, no delays
+    tty.c_cc[VMIN]  = 0;            // read doesn't block
+    tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
 
-        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-                                        // enable reading
-        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-        tty.c_cflag |= parity;
-        tty.c_cflag &= ~CSTOPB;
-        tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                    // enable reading
+    tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+    tty.c_cflag |= parity;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
 
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-        {
-                printf ("error %d from tcsetattr\n", errno);
-                return -1;
-        }
-        return 0;
+    if (tcsetattr (fd, TCSANOW, &tty) != 0)
+    {
+            printf ("error %d from tcsetattr\n", errno);
+            return -1;
+    }
+    return 0;
 }
 
 void
 set_blocking (int fd, int should_block)
 {
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
-        if (tcgetattr (fd, &tty) != 0)
-        {
-                printf ("error %d from tggetattr\n", errno);
-                return;
-        }
+    struct termios tty;
+    memset (&tty, 0, sizeof tty);
+    if (tcgetattr (fd, &tty) != 0)
+    {
+            printf ("error %d from tggetattr\n", errno);
+            return;
+    }
 
-        tty.c_cc[VMIN]  = should_block ? 1 : 0;
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+    tty.c_cc[VMIN]  = should_block ? 1 : 0;
+    tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-                printf ("error %d setting term attributes\n", errno);
+    if (tcsetattr (fd, TCSANOW, &tty) != 0)
+            printf ("error %d setting term attributes\n", errno);
 }
 
 int main()
 {
-        char *portname = "/dev/ttyS0";
+    zsock_t *push = zsock_new_push ("inproc://example");
+    zsock_t *pull = zsock_new_pull ("inproc://example");
+    zstr_send (push, "Hello, World");
 
-        int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
-        if (fd < 0)
-        {
-                        printf("error %d opening %s: %s\n", errno, portname, strerror (errno));
-                        return 0;
-        }
+    char *string = zstr_recv (pull);
+    puts (string);
+    zstr_free (&string);
 
-        // set_interface_attribs (fd, B115200, 0);         // set speed to 115,200 bps, 8n1 (no parity)
-        set_interface_attribs (fd, B9600, 0);         // set speed to 115,200 bps, 8n1 (no parity)
-        set_blocking (fd, 0);                                           // set no blocking
+    zsock_destroy (&pull);
+    zsock_destroy (&push);
 
-        nmea_parser();
+    char *portname = "/dev/ttyS0";
 
-        return 0;
+    int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0)
+    {
+                    printf("error %d opening %s: %s\n", errno, portname, strerror (errno));
+                    return 0;
+    }
+
+    // set_interface_attribs (fd, B115200, 0);       // set speed to 115,200 bps, 8n1 (no parity)
+    set_interface_attribs (fd, B9600, 0);         // set speed to 115,200 bps, 8n1 (no parity)
+    set_blocking (fd, 0);                         // set no blocking
+
+    nmea_parser();
+
+    return 0;
 }
